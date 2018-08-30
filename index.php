009c4cbd365c636e3337d96238003384ed35f4ea
Xheni Myrtaj <myrtajxheni@gmail.com>
2018-08-30 16:57:58 +0200

    add downloadupdate step, still working
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 class updater
 {
 
+
     function checkIfThereIsAnUpdate()
     {
 
@@ -64,9 +65,36 @@ class updater
 
     }
     function downloadUpdate(){
-
+        $url = "http://10.211.55.4/phplisttest";
+        $zipFile = "/../../phpList.zip"; // Local Zip File Path
+        $zipResource = fopen($zipFile, "w");
+// Get The Zip File From Server
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FILE, $zipResource);
+        $page = curl_exec($ch);
+        if(!$page) {
+            echo "Error :- ".curl_error($ch);
+        }
+        curl_close($ch);
+ /** @TODO add this as another step */
+        $zip = new ZipArchive;
+        $extractPath = "../../";
+        if($zip->open($zipFile) != "true"){
+            echo "Error :- Unable to open the Zip File";
+        }
+        /* Extract Zip File */
+        $zip->extractTo($extractPath);
+        $zip->close();
     }
-
 }
 
 $update = new updater();
