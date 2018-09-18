dddd0a9582e87cc7ee6912e18b1bcacd9bdc08da
Xheni Myrtaj <myrtajxheni@gmail.com>
2018-09-18 21:11:35 +0200

    fix download and unzip
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- index.php
+++ index.php
@@ -143,35 +143,44 @@ class updater
     {
     }
 
+    /**
+     *
+     */
     function downloadUpdate()
     {
-        echo "test";
+        echo exec('whoami');
         /** @var string $url */
         $url = "http://10.211.55.7/phplist.zip";
         /** @var ZipArchive $zipFile */
-        $zipPath = '/../phpList.zip'; // Local Zip File Path
-
-        $fp = fopen($zipPath, 'w'); // Get The Zip File From Server
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-
-
+        $zipFile = "phplist.zip"; // Local Zip File Path
+        $zipResource = fopen($zipFile, "w");
+        // Get The Zip File From Server
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FILE, $zipResource);
         $page = curl_exec($ch);
-        if (!$page) {
-            echo "Error :- " . curl_error($ch);
+        if(!$page) {
+            echo "Error :- ".curl_error($ch);
         }
         curl_close($ch);
-        fclose($fp);
-
+        /* Open the Zip file */
         $zip = new ZipArchive;
-        $extractPath = getcwd();
-        if ($zip->open($zipPath) != "true") {
+        $extractPath = "phplist";
+        if($zip->open($zipFile) != "true"){
             echo "Error :- Unable to open the Zip File";
         }
         /* Extract Zip File */
         $zip->extractTo($extractPath);
         $zip->close();
+        die(', phpList was probably extracted, please check.');
 
     }
 }
