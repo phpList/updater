f63490ac65b571a8b58a8956073d1da9feeafaee
Xheni Myrtaj <myrtajxheni@gmail.com>
2018-11-02 15:07:24 +0100

    back up
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- index.php
+++ index.php
@@ -319,6 +319,40 @@ class updater
             opcache_reset();
         }
     }
+
+
+    /**
+     * backUpData('/path/to/folder', '/path/to/backup.zip';
+     * @param $source /path to folder
+     * @param $destination 'path' to backup zip
+     * @return bool
+     */
+    function backUpData($source, $destination) {
+    if (extension_loaded('zip') === true) {
+        if (file_exists($source) === true) {
+            $zip = new ZipArchive();
+            if ($zip->open($destination, ZIPARCHIVE::CREATE) === true) {
+                $source = realpath($source);
+                if (is_dir($source) === true) {
+                    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+                    foreach ($files as $file) {
+                        $file = realpath($file);
+                        if (is_dir($file) === true) {
+                            $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                        } else if (is_file($file) === true) {
+                            $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                        }
+                    }
+                } else if (is_file($source) === true) {
+                    $zip->addFromString(basename($source), file_get_contents($source));
+                }
+            }
+            return $zip->close();
+        }
+    }
+    return false;
+}
+
 }
 
 $update = new updater();
